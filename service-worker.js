20515ce7c4d4ab2d08e243111bce6f467a78838a
zhyswan <www.nova@gmail.com>
2024-09-27 18:33:15 +0800

    alerm

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,33 @@
-// import { createNotification } from "./utils.js";
-chrome.runtime.onInstalled.addListener(() => {
-  console.log("插件已安装");
-
-  // 创建一个简单的通知
-  //   createNotification({
-  //     iconUrl: "images/logo-128.png",
-  //     title: "插件已安装",
-  //     message: "这是一个来自Chrome插件的通知。",
-  //   });
-});
+import { createNotification } from "./utils.js";
+// import { createNotification } from "./web.js";
 
 chrome.action.onClicked.addListener(function () {
   chrome.tabs.create({ url: "popup.html" });
 });
 
-export {};
+chrome.runtime.onInstalled.addListener(() => {
+  console.log("插件已安装");
+  // 创建或重置闹钟
+  chrome.alarms.create("notify", { periodInMinutes: 60 }); // 0.08333 分钟约等于 5 秒
+});
+
+// 监听闹钟事件 浏览器打开就有闹钟事件
+chrome.alarms.onAlarm.addListener((alarm) => {
+  console.log("闹钟已触发", alarm);
+  if (alarm.name === "notify") {
+    createNoti();
+  }
+});
+
+function createNoti() {
+  createNotification({
+    title: "通知标题，必须的，一行",
+    message:
+      "必选 最多显示4行，超出会...显示，如果有contextMessage ，则最多3行",
+    iconUrl: "images/logo-256.png",
+    action: {
+      type: "openUrl",
+      url: "https://www.baidu.com",
+    },
+  });
+}
